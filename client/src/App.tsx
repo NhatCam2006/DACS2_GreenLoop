1f5ea5368b4e8acb01776a95c8e32fc749ee039e
NhatCam2006 <camdn.24ite@vku.udn.vn>
2025-12-07 23:42:46 +0700

    disable TanStack

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 import { useEffect } from "react";
 import { Header } from "./components/layout/Header";
@@ -259,7 +258,6 @@ function App() {
           },
         }}
       />
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
